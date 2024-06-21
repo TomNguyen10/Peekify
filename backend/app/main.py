64fa039f3f076e93c9fd8aaef8678929c66bfbde
TomNguyen10 <dungnguyen_2025@depauw.edu>
2024-06-21 14:47:24 -0400

    ADD: Postgresql connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
+from data.database import Base, engine, SessionLocal
+from sqlalchemy.orm import Session
+
+Base.metadata.create_all(bind=engine)
 
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
@@ -12,3 +26,17 @@ async def root():
 @app.get("/hello/{name}")
 async def hello_name(name: str):
     return {"message": f"Hello {name}"}
+
+
+def get_db() -> Session:
+    """
+    Returns a database session.
+
+    Returns:
+        Session: The database session object.
+    """
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
