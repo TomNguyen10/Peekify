dd4b57e5b03055b6984015e052222d93d471ed75
TomNguyen10 <dungnguyen_2025@depauw.edu>
2025-03-18 22:07:29 -0400

    ADD: Enable SSL for uvicorn server with self-signed certificate

--- backend/app/main.py
+++ backend/app/main.py
@@ -45,4 +45,10 @@ app.include_router(login.router, prefix="")
 app.include_router(top_items.router, prefix="")
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        ssl_keyfile="/etc/ssl/private/selfsigned.key",
+        ssl_certfile="/etc/ssl/certs/selfsigned.crt"
+    )
